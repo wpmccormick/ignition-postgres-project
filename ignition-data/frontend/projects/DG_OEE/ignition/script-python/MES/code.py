2d73e4a71effddd4b2ddcadc707a8b2e4655ed6a
Bill McCormick <wpmccormick@gmail.com>
2024-06-21 13:10:02 -0300

    added cell states & DT table

--- ignition-data/frontend/projects/DG_OEE/ignition/script-python/MES/code.py
+++ ignition-data/frontend/projects/DG_OEE/ignition/script-python/MES/code.py
@@ -1,7 +1,30 @@
-"""Return a list of MES objects of type 'Line' that are not inactive."""
+"""
+MES Helper Functions
+"""
+#Return a list of MES objects of type 'Line' that are not inactive.
 def get_line_list():
 	obj_filter = system.mes.object.filter.createFilter()
 	obj_filter.setMESObjectTypeName('Line')
 	obj_filter.setExcludeInactiveEquipment(True)
 	line_list = system.mes.searchMESObjects(obj_filter)
 	return line_list 
+
+
+#Return a List of Cell Names with their States for a given Line
+def get_cell_states(line):
+	obj_filter = system.mes.object.filter.createFilter()
+	obj_filter.setMESObjectTypeName('LineCell')
+	line_path = fr'Design Group\Sacramento\Information Solutions\{line}'
+	obj_filter.setPrimaryMESObjectPath(line_path)
+	
+	objs = system.mes.searchMESObjects(obj_filter)
+
+	cell_states = []
+	for obj in objs:
+
+		eq_path = obj.getMESObject().getEquipmentPath()
+	
+		cell_state = system.mes.getCurrentEquipmentStates(eq_path)	
+		cell_states.append({'cell_name':obj.getName(),'cell_state':cell_state.values()[0]})
+		
+	return cell_states
