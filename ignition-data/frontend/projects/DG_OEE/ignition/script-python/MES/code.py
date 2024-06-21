d98a14fa8c5d33e4349cf8739dcfbc9bb2f0c326
Bill McCormick <wpmccormick@gmail.com>
2024-06-21 18:58:58 -0300

    fancy cell display

--- ignition-data/frontend/projects/DG_OEE/ignition/script-python/MES/code.py
+++ ignition-data/frontend/projects/DG_OEE/ignition/script-python/MES/code.py
@@ -13,20 +13,23 @@ def get_line_list():
 
 #Return a List of Cell Names with their States for a given Line
 def get_cell_states(line):
-	obj_filter = system.mes.object.filter.createFilter()
-	obj_filter.setMESObjectTypeName('LineCell')
-	line_path = r'Design Group\Sacramento\Information Solutions\%s' % line
-
-	obj_filter.setPrimaryMESObjectPath(line_path)
-	
-	objs = system.mes.searchMESObjects(obj_filter)
-
 	cell_states = []
-	for obj in objs:
-
-		eq_path = obj.getMESObject().getEquipmentPath()
 	
-		cell_state = system.mes.getCurrentEquipmentStates(eq_path)	
-		cell_states.append({'cell_name':obj.getName(),'cell_state':cell_state.values()[0]})
+	if line is not None and line != '':
+	
+		obj_filter = system.mes.object.filter.createFilter()
+		obj_filter.setMESObjectTypeName('LineCell')
+		line_path = r'Design Group\Sacramento\Information Solutions\%s' % line
+	
+		obj_filter.setPrimaryMESObjectPath(line_path)
+		
+		objs = system.mes.searchMESObjects(obj_filter)
+	
+		for obj in objs:
+	
+			eq_path = obj.getMESObject().getEquipmentPath()
+		
+			cell_state = system.mes.getCurrentEquipmentStates(eq_path)	
+			cell_states.append({'cell_name':obj.getName(),'cell_state':cell_state.values()[0]})
 		
 	return cell_states
